038cb8dbd5f1e06c48e556710d55000d4d0a8c1c
xit303 <d.lange303@gmail.com>
2025-07-26 12:15:11 +0200

    refactor: Enhance debug logging in TextProperty for SetText method

--- src/Drawing/Properties/TextProperty.cpp
+++ src/Drawing/Properties/TextProperty.cpp
@@ -1,5 +1,9 @@
 #include <Drawing/Properties/TextProperty.h>
 
+#ifdef DEBUG_TEXT_PROPERTY
+#include <iostream>
+#endif
+
 namespace xit::Drawing
 {
     TextProperty::TextProperty()
@@ -9,11 +13,23 @@ namespace xit::Drawing
 
     void TextProperty::SetText(const std::string &value)
     {
+#ifdef DEBUG_TEXT_PROPERTY
+        std::cout << "[DEBUG] TextProperty::SetText called with: '" << value << "'" << std::endl;
+#endif
         if (text != value)
         {
+#ifdef DEBUG_TEXT_PROPERTY
+            std::cout << "[DEBUG] Text changed, updating from '" << text << "' to '" << value << "'" << std::endl;
+#endif
             text = value;
             needMeasureText = true;
             OnTextChanged();
+        }
+        else
+        {
+#ifdef DEBUG_TEXT_PROPERTY
+            std::cout << "[DEBUG] Text unchanged, not updating" << std::endl;
+#endif
         }
     }
 
