784999e73eae5fcddbd0cf42563b9d0580767ab5
xit303 <d.lange303@gmail.com>
2025-07-30 17:54:13 +0200

    refactor: Add GCC diagnostic push/pop for double promotion warnings in stb_image.h

--- include/OpenGL/stb_image.h
+++ include/OpenGL/stb_image.h
@@ -1,3 +1,6 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdouble-promotion"
+
 /* stb_image - v2.27 - public domain image loader - http://nothings.org/stb
                                   no warranty implied; use at your own risk
 
@@ -8005,3 +8008,5 @@ ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ------------------------------------------------------------------------------
 */
+
+#pragma GCC diagnostic pop
